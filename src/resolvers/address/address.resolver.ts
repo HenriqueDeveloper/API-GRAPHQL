1d6ce2fdf4329243f6cae89f32344f436964cad5
Carlos Henrique <carlossoaressantana@hotmail.com>
2023-01-29 17:14:06 -0300

    feat: add create user

--- src/resolvers/address/address.resolver.ts
+++ src/resolvers/address/address.resolver.ts
@@ -1,11 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateAddressDto } from 'src/dtos/create-address.dto';
 import { Address } from 'src/entities/address.entity';
+import { AddressService } from 'src/services/address/address.service';
 
 @Resolver(() => Address)
 export class AddressResolver {
+  constructor(
+    private addressService: AddressService
+  ) {}
+
+  @Mutation(() => Address)
+  async createAddress(@Args("data") createAddressDto: CreateAddressDto) {
+    return this.addressService.create(createAddressDto)
+  }
     
-    @Query(() => [Address])
-    async addres() {
-      return 'Hello World!';
-    }
+  @Query(() => [Address])
+  async addres() {
+    return this.addressService.findAll();
+  }
 }
--- src/services/address/address.service.ts
+++ src/services/address/address.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateAddressDto } from 'src/dtos/create-address.dto';
+import { Address } from 'src/entities/address.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AddressService {}
+export class AddressService {
+  constructor(
+    @InjectRepository(Address)
+    private repository: Repository<Address>,
+  ) {}
+  
+  async create(createAddressDto: CreateAddressDto): Promise<Address> {
+
+    const address = new Address();
+
+    address.CEP = createAddressDto.CEP;
+    address.city = createAddressDto.city;
+    address.complement = createAddressDto.complement;
+    address.neighborhood = createAddressDto.neighborhood;
+    address.state = createAddressDto.state;
+    address.street = createAddressDto.street;
+    address.streetSituation = createAddressDto.streetSituation;
+    address.typeStreet = createAddressDto.typeStreet;
+    address.number = createAddressDto.number;
+
+    await this.repository.save(address);
+
+    return address;
+  }
+
+  findAll() {
+    return this.repository.find();
+  }
+
+}
